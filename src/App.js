ae2bb8bb6914c8ac9bacecac8c6a45abf34598be
Puspalal <puspalalnewar79@gmail.com>
2024-09-13 21:23:19 +0530

    created interest box

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <h3>Simple Calculator</h3>
+      <div className='box'>
+        <div className="interest-box">
+          <h2>&#8377; 2000</h2>
+        </div>
+      </div>
+
     </div>
   );
 }
