b87df1b5e6ede3f1130d181bdb73ddea6d0051a3
Puspalal <puspalalnewar79@gmail.com>
2024-09-13 21:54:45 +0530

    Completed desig

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <h3>Simple Calculator</h3>
+      <h1>SI Calculator</h1>
       <div className='box'>
         <div className="interest-box">
           <h2>&#8377; 2000</h2>
+        </div>
+        <div className="inputs">
+          <input type="number" placeholder='Principle amount'/>
+          <input type="number" placeholder='Rate of interst(%)'/>
+          <input type="number" placeholder='Time(Year)'/>
+        </div>
+        <div className="buttons">
+          <button className='calc'>Calculate</button>
+          <button className='reset'>Reset</button>
         </div>
       </div>
 
