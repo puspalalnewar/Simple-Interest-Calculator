5af234a249ef6bf9c5723ffa2b21b4573c6edd82
Puspalal <puspalalnewar79@gmail.com>
2024-09-16 13:02:21 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   const amount = useRef(null);
   const rate = useRef(null);
   const time = useRef(null);
-  
-  const calculate = ()=>{
+
+  const calculate = () => {
     let p = amount.current.value;
     let t = time.current.value;
     let r = rate.current.value;
@@ -20,8 +20,17 @@ function App() {
     const interest = (p * t * r) / 100;
     setCurrentInterest(interest);
   }
+  // const calculate = ()=>{
+  //   let p = amount.current.value;
+  //   let t = time.current.value;
+  //   let r = rate.current.value;
 
-  const reset = ()=>{
+  //   // console.log("amount : ", p, "Time : ", t, "Rate : ", r);
+  //   const interest = (p * t * r) / 100;
+  //   setCurrentInterest(interest);
+  // }
+
+  const reset = () => {
     amount.current.value = "";
     rate.current.value = "";
     time.current.value = "";
@@ -38,12 +47,23 @@ function App() {
           <h2>&#8377; {currInterest}</h2>
         </div>
         <div className="inputs">
-          <input ref={amount} type="number" placeholder='Principle amount'/>
-          <input ref={rate} type="number" placeholder='Rate of interst(%)'/>
-          <input ref={time} type="number" placeholder='Time(Year)'/>
+          
+            <label>Enter Amount</label>
+            <br />
+            <input ref={amount} type="number" placeholder='Amount' />
+            <br />
+            <label>Enter Rate</label>
+            <br />
+            <input ref={rate} type="number" placeholder='Rate of interst' />
+            <br />
+            <label>Enter Time</label>
+            <br />
+            <input ref={time} type="number" placeholder='Time' />
+          
+
         </div>
         <div className="buttons">
-          <button onClick={()=> calculate()} className='calc'>Calculate</button>
+          <button onClick={() => calculate()} className='calc'>Calculate</button>
           <button onClick={reset} className='reset'>Reset</button>
         </div>
       </div>
